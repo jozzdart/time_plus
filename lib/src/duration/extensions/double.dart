10a1f0784c2c857e49edf4613311127e3f274751
jozz <yosefd99@gmail.com>
2025-05-23 23:31:42 +0300

    Update double.dart

--- lib/src/duration/extensions/double.dart
+++ lib/src/duration/extensions/double.dart
@@ -1,9 +1,21 @@
 extension DoubleSmartDuration on double {
+  /// Returns a [Duration] object representing the number of days.
   Duration get days => _smartDuration(this, Duration.millisecondsPerDay);
+
+  /// Returns a [Duration] object representing the number of hours.
   Duration get hours => _smartDuration(this, Duration.millisecondsPerHour);
+
+  /// Returns a [Duration] object representing the number of minutes.
   Duration get minutes => _smartDuration(this, Duration.millisecondsPerMinute);
+
+  /// Returns a [Duration] object representing the number of seconds.
   Duration get seconds => _smartDuration(this, Duration.millisecondsPerSecond);
+
+  /// Returns a [Duration] object representing the number of milliseconds.
   Duration get milliseconds => _smartDuration(this, 1);
+
+  /// Returns a [Duration] object representing the number of microseconds.
+  Duration get microseconds => _smartDuration(this, 1000);
 }
 
 /// Decomposes a double [value] into a [Duration] starting from [baseMillis] (e.g. 86400000 for days),
